7f9ff786c0e598fad76e70e37dfa2089f379620e
jérôme GALLEGO <jeromegallego2@gmail.com>
2022-04-03 21:56:00 +0200

    Fix Cart store not included

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import Product from './Product';
+import Cart from './Cart';
 
 const combinedReducer = combineReducers({
     Product,
+    Cart,
 });
 
 const rootReducer = (state, action) => {
--- src/store/saga/index.js
+++ src/store/saga/index.js
@@ -1,7 +1,9 @@
 import { fork } from 'redux-saga/effects';
 
 import Product from './Product';
+import Cart from './Cart';
 
 export default function* rootSaga() {
   yield fork(Product);
+  yield fork(Cart);
 }
