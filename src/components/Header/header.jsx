00a582ee876bdc44e9f1724693afb7515c91a9ad
jérôme GALLEGO <jeromegallego2@gmail.com>
2022-04-03 17:32:00 +0200

    create header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,9 +1,36 @@
+import { NavLink } from 'react-router-dom';
+
+import { FiShoppingCart } from 'react-icons/fi';
+
 import './header.css'
+
+import LOGO from '../../assets/logo.png'
 
 const Header = () => {
     return (
         <header id='header'>
-            header
+            <div className='logo'>
+                <NavLink to={"/"}>
+                    <img src={LOGO} alt="logo" />
+                </NavLink>
+            </div>
+            <div className='menu'>
+                <NavLink to={"/"}>
+                    Inicio
+                </NavLink>
+                <NavLink to={"#"}>
+                    Otra pagina 1
+                </NavLink>
+                <NavLink to={"#"}>
+                    Otra pagina 2
+                </NavLink>
+                <NavLink to={"#"}>
+                    Otra pagina 3
+                </NavLink>
+                <div className='cart'>
+                    <FiShoppingCart className='react-icons' />
+                </div>
+            </div>
         </header>
     )
 }
