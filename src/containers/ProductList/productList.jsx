2bfc2e5796ea738a7f41bcd67cc619e1cf142cf3
jérôme GALLEGO <jeromegallego2@gmail.com>
2022-04-03 18:42:25 +0200

    create product list component

--- src/containers/ProductList/productList.jsx
+++ src/containers/ProductList/productList.jsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import ProductCard from '../../components/ProductCard/productCard'
+import { getProducts } from '../../store/modules/Product/actions'
 import './productList.css'
 
 const baseClass = "product-list"
 
 const ProductList = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getProducts())
+    }, [])
+
+    const products = useSelector((state) => state.Product.products)
+
     return (
         <div className={baseClass}>
-            product list
+            {products.map((product, index) => {
+                return <ProductCard
+                    {...product}
+                    key={`product_${index}`}
+                />
+            })}
         </div>
     )
 }
