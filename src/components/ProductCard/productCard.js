cf4d3d9f6ea194e4eab347fb9dc3ab85438ff948
jérôme GALLEGO <jeromegallego2@gmail.com>
2022-04-03 17:56:55 +0200

    create productCard component

---
+++ src/components/ProductCard/productCard.js
@@ -0,0 +1,13 @@
+import './productCard.css'
+
+const baseClass = 'product-card'
+
+const ProductCard = () => {
+    return (
+        <div className={baseClass}>
+            product card
+        </div>
+    )
+}
+
+export default ProductCard
