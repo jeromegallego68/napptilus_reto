f58b4aa7100fdbd668a94dc63648d03a024b75b9
jérôme GALLEGO <jeromegallego2@gmail.com>
2022-04-03 19:02:56 +0200

    create product card

--- src/components/ProductCard/productCard.js
+++ src/components/ProductCard/productCard.js
@@ -1,13 +1,47 @@
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+
 import './productCard.css'
 
 const baseClass = 'product-card'
 
-const ProductCard = () => {
+const ProductCard = (
+    data
+) => {
+    const navigate = useNavigate();
+    const { id, brand, imgUrl, model, price } = data
+    const currency = "€"
+
+
     return (
-        <div className={baseClass}>
-            product card
+        <div className={baseClass} onClick={() => navigate(`/product/${id}`)}>
+            <div className='image' style={{
+                backgroundImage: `url("${imgUrl}")`
+            }}>
+            </div>
+            <div className='content'>
+                <div className='infos'>
+                    <div className='brand'>
+                        {brand}
+                    </div>
+                    <div className='model'>
+                        {model}
+                    </div>
+                </div>
+                <div className='price'>
+                    {`${price} ${price ? currency : ''}`}
+                </div>
+            </div>
         </div>
     )
+}
+
+ProductCard.propTypes = {
+    id: PropTypes.string,
+    brand: PropTypes.string,
+    imgUrl: PropTypes.string,
+    model: PropTypes.string,
+    price: PropTypes.string
 }
 
 export default ProductCard
