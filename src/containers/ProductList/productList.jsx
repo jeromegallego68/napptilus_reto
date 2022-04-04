635c7756a69915d4e908b6794ea336f0db293a73
jérôme GALLEGO <jeromegallego2@gmail.com>
2022-04-04 21:07:14 +0200

    create search button

--- src/containers/ProductList/productList.jsx
+++ src/containers/ProductList/productList.jsx
@@ -1,28 +1,62 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import ProductCard from '../../components/ProductCard/productCard'
 import { getProducts } from '../../store/modules/Product/actions'
+import { AiOutlineSearch } from 'react-icons/ai'
 import './productList.css'
 
 const baseClass = "product-list"
 
 const ProductList = () => {
     const dispatch = useDispatch()
+    const [productsFiltered, setProductsFiltered] = useState([])
+
+    const products = useSelector((state) => state.Product.products)
 
     useEffect(() => {
         dispatch(getProducts())
     }, [])
 
-    const products = useSelector((state) => state.Product.products)
+    useEffect(() => {
+        setProductsFiltered(products)
+    }, [products])
+
+    const handleSearchProduct = (e) => {
+        const { value } = e.target
+
+        let productsFiltered = products.filter(product => {
+
+            let modelLowercase = product.model.toLowerCase()
+            let brandLowercase = product.brand.toLowerCase()
+            let valueLowercase = value.toLowerCase()
+
+            return modelLowercase.includes(valueLowercase) || brandLowercase.includes(value)
+        })
+        setProductsFiltered(productsFiltered)
+    }
 
     return (
         <div className={baseClass}>
-            {products.map((product, index) => {
-                return <ProductCard
-                    {...product}
-                    key={`product_${index}`}
-                />
-            })}
+            <div className={`${baseClass}__head`}>
+                <div className='search'>
+                    <input
+                        type="text"
+                        placeholder='Busca tu producto por marca o modelo'
+                        onChange={handleSearchProduct}
+                    />
+
+                    <AiOutlineSearch className='react-icons' />
+                </div>
+            </div>
+            <div className={`${baseClass}__container`}>
+                {productsFiltered.map((product, index) => {
+                    return <ProductCard
+                        {...product}
+                        key={`product_${index}`}
+                    />
+                })}
+
+            </div>
         </div>
     )
 }
