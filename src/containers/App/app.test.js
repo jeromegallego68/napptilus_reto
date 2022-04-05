6f2d1afa5087409535ae621ef8d7f193a4ba95b3
jérôme GALLEGO <jeromegallego2@gmail.com>
2022-04-05 19:42:30 +0200

    create test for App container

--- src/containers/App/app.test.js
+++ src/containers/App/app.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './app';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import configureStore from 'redux-mock-store';
+
+describe('With React Testing Library', () => {
+	const mockStore = configureStore();
+	let store = mockStore({
+		Cart: {},
+		Product: {}
+	});
+
+	it('Render header', () => {
+		render(<Provider store={store}><App /></Provider>);
+
+		const header = screen.getByRole("banner");
+		expect(header).toBeInTheDocument();
+	});
+
+	it('Render footer', () => {
+		render(<Provider store={store}><App /></Provider>);
+
+		const footer = screen.getByTestId("footer");
+		expect(footer).toBeInTheDocument();
+	});
 });
